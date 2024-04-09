90014995c4aae18aea6b6116fcbdda43380fcdb9
Ritchie-Robinson <78422233+Ritchie-Robinson@users.noreply.github.com>
2024-04-09 16:09:41 +0100

    Add Chatroom component
    Add Chatroom component and imported to App

--- chatroom/src/App.js
+++ chatroom/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
+import Chatroom from './components/Chatroom';
 
 function App() {
   return (
     <div className="App">
      <Login />
+     <Chatroom />
     </div>
   );
 }
