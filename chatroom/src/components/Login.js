2a3afb24639c4deee6be315b7f96d1ab054b5133
Ritchie-Robinson <78422233+Ritchie-Robinson@users.noreply.github.com>
2024-04-10 13:06:55 +0100

    Added login design

--- chatroom/src/components/Login.js
+++ chatroom/src/components/Login.js
@@ -1,8 +1,35 @@
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "../firebase";
 
 function Login() {
     return (
-        <div>
-            <h1>Login</h1>
+        <div style={{ display: 'flex', flex: 1, height: '100vh'}}>
+            <div style={{
+                display: 'flex',
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+                minWidth: 300
+            }}>
+                <button style={{
+                    background: 'red',
+                    borderWidth: 0,
+                    paddingTop: 12,
+                    paddingBottom: 12,
+                    paddingLeft: 24,
+                    paddingRight: 24,
+                    color: 'white',
+                    fontSize: 18,
+                    fontWeight: 'bold'
+                }} onClick={async () => {
+                    const provider = new GoogleAuthProvider();
+                    const result = await signInWithPopup(auth, provider);
+                    const credential = GoogleAuthProvider.credentialFromResult(result);
+                    const token = credential.accessToken;
+                    const user = result.user;
+                    console.log(user);
+                }}>Login with Google</button>
+            </div>
         </div>
     );
 }
